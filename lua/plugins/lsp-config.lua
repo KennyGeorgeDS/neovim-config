15dda5d31718ac30a1c07a19c8f741687d575e5f
KennyGeorgeDS <kenny.george@acremaps.com>
2024-01-31 19:40:02 -0600

    still messing with LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,50 +1,108 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = function()
-            require("mason").setup()
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "bashls",
-                    "dockerls",
                     "html",
                     "lua_ls",
                     "pyright",
-                    "sqlls",
                 }
             })
         end,
-        --lazy = false,
-        --opts = {
-        --    automatic_installalation = true,
-        --},
+    lazy = false,
+    opts = {
+      auto_install = true,
     },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        config = function()
-            local capabilities = require('cmp_nvim_lsp').default_capabilities()
-            local lspconfig = require("lspconfig")
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-            -- LSP Setup
-            lspconfig.bashls.setup({capabilities = capabilities})
-            lspconfig.dockerls.setup({capabilities = capabilities})
-            lspconfig.html.setup({capabilities = capabilities})
-            lspconfig.lua_ls.setup({capabilities = capabilities})
-            lspconfig.pyright.setup({capabilities = capabilities})
-            lspconfig.sqlls.setup({capabilities = capabilities})
+      local lspconfig = require("lspconfig")
+      lspconfig.tsserver.setup({
+        capabilities = capabilities
+      })
+      lspconfig.html.setup({
+        capabilities = capabilities
+      })
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities
+      })
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-        end,
-    },
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
+
+
+
+
+
+
+
+
+
+
+--return {
+--    {
+--        "williamboman/mason.nvim",
+--        lazy = false,
+--        config = function()
+--            require("mason").setup()
+--        end,
+--    },
+--    {
+--        "williamboman/mason-lspconfig.nvim",
+--        config = function()
+--            require("mason-lspconfig").setup({
+--                ensure_installed = {
+--                    "bashls",
+--                    "dockerls",
+--                    "html",
+--                    "lua_ls",
+--                    "pyright",
+--                    "sqlls",
+--                }
+--            })
+--        end,
+--        --lazy = false,
+--        --opts = {
+--        --    automatic_installalation = true,
+--        --},
+--    },
+--    {
+--        "neovim/nvim-lspconfig",
+--        lazy = false,
+--        config = function()
+--            local capabilities = require('cmp_nvim_lsp').default_capabilities()
+--            local lspconfig = require("lspconfig")
+--
+--            -- LSP Setup
+--            lspconfig.bashls.setup({capabilities = capabilities})
+--            lspconfig.dockerls.setup({capabilities = capabilities})
+--            lspconfig.html.setup({capabilities = capabilities})
+--            lspconfig.lua_ls.setup({capabilities = capabilities})
+--            lspconfig.pyright.setup({capabilities = capabilities})
+--            lspconfig.sqlls.setup({capabilities = capabilities})
+--
+--            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+--            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+--            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+--            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+--        end,
+--    },
+--}
 
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,48 +1,136 @@
 return {
-    {
-        "jay-babu/mason-null-ls.nvim",
-        --event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "nvimtools/none-ls.nvim",
-        },
-        config = function()
-            local mason_null_ls = require("mason-null-ls")
-            mason_null_ls.setup({
-                ensure_installed = {
-                    "stylua",
-                    "black",
-                    "isort",
-                    "sqlfmt",
-                    "sqlfluff",
-                    "prettier",
-                },
-                automatic_installed = true
-            })
-        end,
-    },
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            local null_ls = require("null-ls")
-            null_ls.setup({
-                sources = {
-                    null_ls.builtins.formatting.stylua,
-                    null_ls.builtins.formatting.black,
-                    null_ls.builtins.formatting.isort,
-                    --null_ls.builtins.formatting.isort.with({ extra_args = { "--profile", "black" } }),
-                    null_ls.builtins.formatting.sqlfmt,
-                    --null_ls.builtins.formatting.sqlfluff,
-                    null_ls.builtins.diagnostics.sqlfluff.with({
-                        extra_args = { "--dialect", "postgres" }, -- change to your dialect
-                    }),
-                    null_ls.builtins.formatting.prettier,
-                    --null_ls.builtins.diagnostics.erb_lint,
-                    --null_ls.builtins.diagnostics.eslint_d,
-                },
-            })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.erb_lint,
+				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.diagnostics.rubocop,
+				null_ls.builtins.formatting.rubocop,
+			},
+		})
 
-            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-        end
-    },
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
+
+
+
+
+
+
+
+
+
+
+
+
+--return {
+--  {
+--    "williamboman/mason.nvim",
+--    lazy = false,
+--    config = function()
+--      require("mason").setup()
+--    end,
+--  },
+--  {
+--    "williamboman/mason-lspconfig.nvim",
+--    lazy = false,
+--    opts = {
+--      auto_install = true,
+--    },
+--  },
+--  {
+--    "neovim/nvim-lspconfig",
+--    lazy = false,
+--    config = function()
+--      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+--
+--      local lspconfig = require("lspconfig")
+--      lspconfig.tsserver.setup({
+--        capabilities = capabilities
+--      })
+--      lspconfig.html.setup({
+--        capabilities = capabilities
+--      })
+--      lspconfig.lua_ls.setup({
+--        capabilities = capabilities
+--      })
+--
+--      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+--      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+--      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+--      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+--    end,
+--  },
+--}
+
+
+
+
+
+
+
+
+
+
+
+--return {
+--    {
+--        "jay-babu/mason-null-ls.nvim",
+--        --event = { "BufReadPre", "BufNewFile" },
+--        dependencies = {
+--            "williamboman/mason.nvim",
+--            "nvimtools/none-ls.nvim",
+--        },
+--        config = function()
+--            local mason = require("mason")
+--            local mason_null_ls = require("mason-null-ls")
+--
+--            mason.setup({
+--                ensure_installed = {
+--                    "black"
+--                },
+--            })
+--
+--            mason_null_ls.setup({
+--                ensure_installed = {
+--                    "stylua",
+--                    "black",
+--                    --"isort",
+--                    --"sqlfmt",
+--                    --"sqlfluff",
+--                    "prettier",
+--                },
+--                automatic_installed = true
+--            })
+--        end,
+--    },
+--    {
+--        "nvimtools/none-ls.nvim",
+--        config = function()
+--            local null_ls = require("null-ls")
+--            null_ls.setup({
+--                sources = {
+--                    null_ls.builtins.formatting.stylua,
+--                    null_ls.builtins.formatting.black,
+--                    --null_ls.builtins.formatting.isort,
+--                    --null_ls.builtins.formatting.isort.with({ extra_args = { "--profile", "black" } }),
+--                    --null_ls.builtins.formatting.sqlfmt,
+--                    --null_ls.builtins.formatting.sqlfluff,
+--                    --null_ls.builtins.diagnostics.sqlfluff.with({
+--                    --    extra_args = { "--dialect", "postgres" }, -- change to your dialect
+--                    --}),
+--                    null_ls.builtins.formatting.prettier,
+--                    --null_ls.builtins.diagnostics.erb_lint,
+--                    --null_ls.builtins.diagnostics.eslint_d,
+--                },
+--            })
+--
+--            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+--        end
+--    },
+--}
